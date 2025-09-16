7a8a51aa6ca19adeb0b4a6f65b7794f22f28a65d
andi-ra <adulla95@gmail.com>
2025-09-16 23:50:45 +0200

    passed day 1

--- exercises/day01/exercise.test.js
+++ exercises/day01/exercise.test.js
@@ -6,14 +6,15 @@ describe('normalizeUrl', () => {
     expect(normalizeUrl('http://a/', '/b')).toBe('http://a/b');
     expect(normalizeUrl('http://a', 'b')).toBe('http://a/b');
     expect(normalizeUrl('http://a//', '//b')).toBe('http://a/b');
+    expect(normalizeUrl('http://a//', 'b?x=1#y')).toBe('http://a/b?x=1#y');
+    expect(normalizeUrl('http://a//', '')).toBe('http://a');
+
   });
 });
 
 describe('pickHeaders', () => {
   it('extracts with defaults', () => {
-    expect(pickHeaders({ Age: '12', 'X-Cache': 'HIT' }))
-      .toEqual({ age: 12, xCache: 'HIT' });
-    expect(pickHeaders({}))
-      .toEqual({ age: 0, xCache: 'BYPASS' });
+    expect(pickHeaders({})).toEqual({ age: 0, xCache: 'BYPASS' });
+    expect(pickHeaders({ Age: '12', 'X-Cache': 'HIT' })).toEqual({ age: 12, xCache: 'HIT' });
   });
 });
